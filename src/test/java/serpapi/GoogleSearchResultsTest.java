76531060ad99dfc1094b3ccdf00a9d7381652227
jvmvik <jvmvik@gmail.com>
2018-02-09 11:17:50 -0600

    Fix number of organic results found

--- src/test/java/serpapi/GoogleSearchResultsTest.java
+++ src/test/java/serpapi/GoogleSearchResultsTest.java
@@ -56,7 +56,7 @@ public class GoogleSearchResultsTest
     GoogleSearchResults serp = new GoogleSearchResults(parameter);
     JsonObject results = serp.getJson();
 
-    assertEquals(3, results.getAsJsonArray("organic_results").size());
+    assertEquals(9, results.getAsJsonArray("organic_results").size());
   }
 
   @Test
